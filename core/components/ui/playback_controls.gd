8dc16a05a4d5fe7425412594b31fa6d5d9cf29a7
Foolminate <77478322+Foolminate@users.noreply.github.com>
2026-02-07 23:34:36 +1300

    feat: time progress labels

--- core/components/ui/playback_controls.gd
+++ core/components/ui/playback_controls.gd
@@ -12,6 +12,8 @@ signal on_speed_changed(new_speed: float)
 @onready var timeline_slider: HSlider = %TimelineSlider
 @onready var speed_selector: SpinBox = %SpeedSelector
 @onready var stop_button: Button = %StopButton
+@onready var elapsed_time_label: Label = %ElapsedTimeLabel
+@onready var total_time_label: Label = %TotalTimeLabel
 
 var is_playing: bool = false
 
@@ -25,14 +27,18 @@ func set_playback_speed(speed: float) -> void:
 
 func update_timeline_range(max_steps: int) -> void:
 	timeline_slider.max_value = max_steps - 1
+	total_time_label.text = _format_time(max_steps)
 
 func update_current_step(index: int) -> void:
 	timeline_slider.set_value_no_signal(index)
+	elapsed_time_label.text = _format_time(index)
 
 func change_timeline_range(current_index: int, total_steps: int) -> void:
 	timeline_slider.min_value = -1
 	timeline_slider.max_value = total_steps - 1
 	timeline_slider.set_value_no_signal(current_index)
+	elapsed_time_label.text = _format_time(current_index)
+	total_time_label.text = _format_time(total_steps)
 
 func _play() -> void:
 	play_pause_button.text = "Pause"
@@ -60,7 +66,7 @@ func _ready() -> void:
 	rewind_button.pressed.connect(_reverse)
 	timeline_slider.value_changed.connect(func(value): on_step_requested.emit(int(value)))
 	timeline_slider.drag_ended.connect(func(_v): timeline_slider.release_focus())
-	speed_selector.value_changed.connect(func(value): on_speed_changed.emit(value))
+	speed_selector.value_changed.connect(func(value): _on_speed_changed(value))
 
 	set_playback_state(is_playing)
 
@@ -71,3 +77,15 @@ func _on_play_pause_toggled() -> void:
 	else:
 		on_play_pressed.emit()
 		_play()
+
+func _on_speed_changed(value: float) -> void:
+	on_speed_changed.emit(value)
+	elapsed_time_label.text = _format_time(int(timeline_slider.value))
+	total_time_label.text = _format_time(int(timeline_slider.max_value))
+
+func _format_time(step: int) -> String:
+	var total_seconds = int((step + 1) / speed_selector.value)
+	@warning_ignore("integer_division")
+	var minutes = total_seconds / 60
+	var seconds = total_seconds % 60
+	return "%02d:%02d" % [minutes, seconds]
