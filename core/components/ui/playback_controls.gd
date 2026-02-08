6bde924747b57749a92a50b09266eb5ce151b949
Foolminate <77478322+Foolminate@users.noreply.github.com>
2026-02-08 14:24:12 +1300

    feat: icons, stepping, and fast forward

--- core/components/ui/playback_controls.gd
+++ core/components/ui/playback_controls.gd
@@ -7,13 +7,24 @@ signal on_rewind_pressed()
 signal on_step_requested(index: int)
 signal on_speed_changed(new_speed: float)
 
+# Buttons
 @onready var rewind_button: Button = %RewindButton
+@onready var step_backward_button: Button = %StepBackwardButton
 @onready var play_pause_button: Button = %PlayPauseButton
-@onready var timeline_slider: HSlider = %TimelineSlider
-@onready var speed_selector: SpinBox = %SpeedSelector
+@onready var step_forward_button: Button = %StepForwardButton
+@onready var fast_forward_button: Button = %FastForwardButton
 @onready var stop_button: Button = %StopButton
+# Timeline
 @onready var elapsed_time_label: Label = %ElapsedTimeLabel
+@onready var elapsed_frames_label: Label = %ElapsedFramesLabel
+@onready var timeline_slider: HSlider = %TimelineSlider
 @onready var total_time_label: Label = %TotalTimeLabel
+@onready var total_frames_label: Label = %TotalFramesLabel
+# Playback Speed
+@onready var speed_selector: SpinBox = %SpeedSelector
+
+@export var play_icon: Texture2D
+@export var pause_icon: Texture2D
 
 var is_playing: bool = false
 
@@ -28,42 +39,66 @@ func set_playback_speed(speed: float) -> void:
 func update_timeline_range(max_steps: int) -> void:
 	timeline_slider.max_value = max_steps - 1
 	total_time_label.text = _format_time(max_steps)
+	total_frames_label.text = str(max_steps)
 
 func update_current_step(index: int) -> void:
 	timeline_slider.set_value_no_signal(index)
 	elapsed_time_label.text = _format_time(index)
+	elapsed_frames_label.text = str(index + 1)
 
 func change_timeline_range(current_index: int, total_steps: int) -> void:
 	timeline_slider.min_value = -1
 	timeline_slider.max_value = total_steps - 1
 	timeline_slider.set_value_no_signal(current_index)
 	elapsed_time_label.text = _format_time(current_index)
+	elapsed_frames_label.text = str(current_index + 1)
 	total_time_label.text = _format_time(total_steps)
-
-func _play() -> void:
-	play_pause_button.text = "Pause"
-	play_pause_button.set_pressed_no_signal(true)
-	play_pause_button.release_focus()
-
-func _pause() -> void:
-	play_pause_button.text = "Play"
-	play_pause_button.release_focus()
-	play_pause_button.set_pressed_no_signal(false)
-
-func _stop() -> void:
-	_pause()
-	on_stop_pressed.emit()
-	stop_button.release_focus()
+	total_frames_label.text = str(total_steps)
 
 func _reverse() -> void:
 	on_rewind_pressed.emit()
 	rewind_button.release_focus()
 	play_pause_button.set_pressed_no_signal(true)
 
+func _step_backward() -> void:
+	_pause()
+	on_step_requested.emit(int(timeline_slider.value) - 1)
+	step_backward_button.release_focus()
+
+func _play() -> void:
+	play_pause_button.icon = pause_icon
+	play_pause_button.set_pressed_no_signal(true)
+	play_pause_button.release_focus()
+
+func _pause() -> void:
+	play_pause_button.icon = play_icon
+	play_pause_button.release_focus()
+	play_pause_button.set_pressed_no_signal(false)
+
+func _step_forward() -> void:
+	_pause()
+	on_step_requested.emit(int(timeline_slider.value) + 1)
+	step_forward_button.release_focus()
+
+func _fast_forward() -> void:
+	speed_selector.value *= 2
+	if not is_playing:
+		on_play_pressed.emit()
+		_play()
+	fast_forward_button.release_focus()
+
+func _stop() -> void:
+	_pause()
+	on_stop_pressed.emit()
+	stop_button.release_focus()
+
 func _ready() -> void:
-	play_pause_button.pressed.connect(_on_play_pause_toggled)
-	stop_button.pressed.connect(_stop)
 	rewind_button.pressed.connect(_reverse)
+	step_backward_button.pressed.connect(_step_backward)
+	play_pause_button.pressed.connect(_on_play_pause_toggled)
+	step_forward_button.pressed.connect(_step_forward)
+	fast_forward_button.pressed.connect(_fast_forward)
+	stop_button.pressed.connect(_stop)
 	timeline_slider.value_changed.connect(func(value): on_step_requested.emit(int(value)))
 	timeline_slider.drag_ended.connect(func(_v): timeline_slider.release_focus())
 	speed_selector.value_changed.connect(func(value): _on_speed_changed(value))
